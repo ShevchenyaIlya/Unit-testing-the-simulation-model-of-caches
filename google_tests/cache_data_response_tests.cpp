d81fd1fde80fc0096d48b5fbc320434a7f9ffc2f
ShevchenyaIlya <shevchenya.i@gmail.com>
2020-11-15 22:08:14 +0300

    google_tests/cache_data_response_tests.cpp

---
+++ google_tests/cache_data_response_tests.cpp
@@ -0,0 +1,18 @@
+//
+// Created by shevchenya on 15/11/2020.
+//
+
+#include <random>
+
+#include "gtest/gtest.h"
+#include "../src/Memory.h"
+
+class CacheMemoryFixture : public ::testing::Test {
+protected:
+    void SetUp() override {
+        MemoryStorage mem ;
+        UncachedMem uncachedMem = UncachedMem (mem);
+        memModelPtr = std::make_unique<CachedMem> ( uncachedMem);
+    }
+    std::unique_ptr<CachedMem> memModelPtr;
+};
