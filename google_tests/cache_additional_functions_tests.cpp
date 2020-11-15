41ed1c8c1442d5f3d20f865a8cf36f374820ffe9
ShevchenyaIlya <shevchenya.i@gmail.com>
2020-11-15 22:06:50 +0300

    Added google tests for secondary functions in Cache class

--- google_tests/cache_additional_functions_tests.cpp
+++ google_tests/cache_additional_functions_tests.cpp
@@ -3,6 +3,7 @@
 //
 
 #include <memory>
+#include <random>
 
 #include "gtest/gtest.h"
 #include "../src/Memory.h"
@@ -65,16 +66,73 @@ TEST_F(CacheMemoryFixture, CheckAddressTests) {
 
 
 TEST_F(CacheMemoryFixture, CheckAllBitsTests) {
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<> distrib(0, blockCount);
     CachedMem cache = *memModelPtr;
-    ASSERT_TRUE(cache.checkAllBits(cache.getCodeMemory(), 512, 12));
-    ASSERT_TRUE(cache.checkAllBits(cache.getDataMemory(), 512, 12));
+
+    ASSERT_FALSE(cache.checkAllBits(cache.getCodeMemory(), distrib(gen), (512 % (codeCacheBytes / blockCount)) / lineSizeBytes));
+    ASSERT_FALSE(cache.checkAllBits(cache.getDataMemory(), distrib(gen), (640 % (dataCacheBytes / blockCount)) / lineSizeBytes));
+
+    int blockId = distrib(gen);
+    for (Block &block : cache.getCodeMemory()) {
+        block[blockId].lastUsage = 1;
+    }
+
+    ASSERT_TRUE(cache.checkAllBits(cache.getCodeMemory(), distrib(gen), blockId));
+
+    blockId = distrib(gen);
+    for (Block &block : cache.getDataMemory()) {
+        block[blockId].lastUsage = 1;
+    }
+
+    ASSERT_TRUE(cache.checkAllBits(cache.getDataMemory(), distrib(gen), blockId));
+
+    cache.getCodeMemory()[2][3].lastUsage = 0;
+    ASSERT_FALSE(cache.checkAllBits(cache.getCodeMemory(), 0, 0));
+
+    cache.getDataMemory()[3][2].lastUsage = 0;
+    ASSERT_FALSE(cache.checkAllBits(cache.getCodeMemory(), 0, 0));
+
 }
 
 
 TEST_F(CacheMemoryFixture, ChangeLRUBitTests) {
     CachedMem cache = *memModelPtr;
-    ASSERT_TRUE(cache.checkAllBits(cache.getCodeMemory(), 512, 12));
-    ASSERT_TRUE(cache.checkAllBits(cache.getDataMemory(), 512, 12));
+
+    CachedMem::changeLRUBit(cache.getDataMemory(), 0, 0);
+    for (Block block : cache.getDataMemory()) {
+        ASSERT_EQ(block[0].lastUsage, 0);
+    }
+
+    CachedMem::changeLRUBit(cache.getCodeMemory(), 1, 1);
+    for (Block block : cache.getDataMemory()) {
+        ASSERT_EQ(block[1].lastUsage, 0);
+    }
+
+    for (Block &block : cache.getDataMemory()) {
+        block[2].lastUsage = 1;
+    }
+
+    for (Block &block : cache.getCodeMemory()) {
+        block[3].lastUsage = 1;
+    }
+
+    CachedMem::changeLRUBit(cache.getDataMemory(), 0, 2);
+    for (int i = 0; i < cache.getDataMemory().size(); i++) {
+        if (i == 0)
+            ASSERT_EQ(cache.getDataMemory()[i][2].lastUsage, 1);
+        else
+            ASSERT_EQ(cache.getDataMemory()[i][2].lastUsage, 0);
+    }
+
+    CachedMem::changeLRUBit(cache.getCodeMemory(), 1, 3);
+    for (int i = 0; i < cache.getCodeMemory().size(); i++) {
+        if (i == 1)
+            ASSERT_EQ(cache.getCodeMemory()[i][3].lastUsage, 1);
+        else
+            ASSERT_EQ(cache.getCodeMemory()[i][3].lastUsage, 0);
+    }
 }
 
 
