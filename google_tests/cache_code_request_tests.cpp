6375ec82e49b068b778c3cb33094a5125ef375d3
ShevchenyaIlya <shevchenya.i@gmail.com>
2020-11-16 23:12:22 +0300

    google_tests/cache_code_request_tests.cpp Testing requesting to code cache

--- google_tests/cache_code_request_tests.cpp
+++ google_tests/cache_code_request_tests.cpp
@@ -16,3 +16,37 @@ protected:
     }
     std::unique_ptr<CachedMem> memModelPtr;
 };
+
+TEST_F(CacheMemoryFixture, waitRequest) {
+    CachedMem cache = *memModelPtr;
+
+    cache.setMemoryRequestIp(512);
+    cache.Request(512);
+    ASSERT_EQ(cache.getRequestedIp(), 0);
+    ASSERT_EQ(cache.getRequestedOffset(), 0);
+}
+
+TEST_F(CacheMemoryFixture, lineInCache) {
+    CachedMem cache = *memModelPtr;
+
+    int blockLine = rand() % blockCount;
+    Word requestedId = rand() % 1024;
+    cache.getCodeMemory()[blockLine][requestedId % (codeCacheBytes / blockCount) / lineSizeBytes].address = ToLineAddr(requestedId);
+    cache.Request(requestedId);
+    ASSERT_FALSE(cache.isCacheMiss());
+    ASSERT_EQ(cache.getWaitCycles(), cache.getCodeLatency());
+    ASSERT_EQ(cache.getRequestedIp(), ToLineAddr(requestedId));
+    ASSERT_EQ(cache.getRequestedOffset(), ToLineOffset(requestedId));
+}
+
+TEST_F(CacheMemoryFixture, lineNotInCache) {
+    CachedMem cache = *memModelPtr;
+
+    int blockLine = rand() % blockCount;
+    Word requestedId = rand() % 1024;
+    cache.Request(requestedId);
+    ASSERT_TRUE(cache.isCacheMiss());
+    ASSERT_EQ(cache.getWaitCycles(), cache.getFailLatency());
+    ASSERT_EQ(cache.getRequestedIp(), ToLineAddr(requestedId));
+    ASSERT_EQ(cache.getRequestedOffset(), ToLineOffset(requestedId));
+}
