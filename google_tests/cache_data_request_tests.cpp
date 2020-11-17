c6f47c66ca4166b67ddbe8e05c79c037aad68d42
ShevchenyaIlya <shevchenya.i@gmail.com>
2020-11-17 10:21:32 +0300

    google_tests/cache_data_request_tests

--- google_tests/cache_data_request_tests.cpp
+++ google_tests/cache_data_request_tests.cpp
@@ -6,7 +6,8 @@
 
 #include "gtest/gtest.h"
 #include "../src/Memory.h"
-#include "../src/PoolAllocator.h"
+#include "../src/Instruction.h"
+#include "../src/Decoder.h"
 
 class CacheMemoryFixture : public ::testing::Test {
 protected:
@@ -18,3 +19,48 @@ protected:
     std::unique_ptr<CachedMem> memModelPtr;
 };
 
+TEST_F(CacheMemoryFixture, notLoadOrStoreCommands) {
+    CachedMem cache = *memModelPtr;
+
+    cache.setMemoryRequestIp(512);
+    cache.Request(512, IType::J);
+    ASSERT_EQ(cache.getRequestedIp(), 0);
+    ASSERT_EQ(cache.getRequestedOffset(), 0);
+    ASSERT_EQ(cache.getWaitCycles(), 0);
+
+    cache.Request(512, IType::Unsupported);
+    ASSERT_EQ(cache.getRequestedIp(), 0);
+    ASSERT_EQ(cache.getRequestedOffset(), 0);
+    ASSERT_EQ(cache.getWaitCycles(), 0);
+}
+
+TEST_F(CacheMemoryFixture, cacheHit) {
+    CachedMem cache = *memModelPtr;
+
+    int blockLine = rand() % blockCount;
+    Word requestedId = 516;
+    cache.getDataMemory()[blockLine][requestedId % (dataCacheBytes / blockCount) / lineSizeBytes].address = ToLineAddr(requestedId);
+    cache.Request(requestedId, IType::St);
+    ASSERT_FALSE(cache.isCacheMiss());
+    ASSERT_EQ(cache.getWaitCycles(), cache.getDataLatency());
+    ASSERT_EQ(cache.getRequestedIp(), ToLineAddr(requestedId));
+    ASSERT_EQ(cache.getRequestedOffset(), ToLineOffset(requestedId));
+}
+
+TEST_F(CacheMemoryFixture, requestMissStoreCommand) {
+    CachedMem cache = *memModelPtr;
+
+    int blockLine = rand() % blockCount;
+    Word requestedId = 520;
+    cache.Request(requestedId, IType::Ld);
+    ASSERT_TRUE(cache.isCacheMiss());
+    ASSERT_EQ(cache.getWaitCycles(), cache.getFailLatency());
+    ASSERT_EQ(cache.getRequestedIp(), ToLineAddr(requestedId));
+    ASSERT_EQ(cache.getRequestedOffset(), ToLineOffset(requestedId));
+
+    cache.Request(requestedId, IType::St);
+    ASSERT_TRUE(cache.isCacheMiss());
+    ASSERT_EQ(cache.getWaitCycles(), cache.getFailLatency());
+    ASSERT_EQ(cache.getRequestedIp(), ToLineAddr(requestedId));
+    ASSERT_EQ(cache.getRequestedOffset(), ToLineOffset(requestedId));
+}
